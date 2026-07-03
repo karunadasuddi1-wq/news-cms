44412c8d86d8831c3b5a55eef8768746269a9c10
karunadasuddi1-wq <karunadasuddi1@gmail.com>
2026-07-03 07:10:01 +0000

    Fix duplicate AiUsageLog in models/index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -5,7 +5,6 @@ const Article = require('./Article');
 const Page = require('./Page');
 const Setting = require('./Setting');
 const AiUsageLog = require('./AiUsageLog');
-const AiUsageLog = require('./AiUsageLog');
 
 // Article associations
 Article.belongsTo(User, { as: 'author', foreignKey: { name: 'authorId', field: 'author_id', allowNull: false } });
@@ -16,7 +15,6 @@ Category.hasMany(Article, { as: 'articles', foreignKey: { name: 'categoryId', fi
 
 module.exports = {
   Setting,
-  AiUsageLog,
   AiUsageLog,
   sequelize,
   User,
